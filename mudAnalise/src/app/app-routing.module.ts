8cfe1cde1efb716da58df7941080da8001edd512
Pedro Muramatsu <pedromuramatsuc@gmail.com>
2019-04-23 09:34:42 -0300

    Esboco Telas MudAnalise
    23.04.2019 LDI

--- mudAnalise/src/app/app-routing.module.ts
+++ mudAnalise/src/app/app-routing.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+  { path: '', loadChildren: './login/login.module#LoginPageModule' },
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'auth', loadChildren: './auth/auth.module#AuthPageModule' }
 ];
 @NgModule({
   imports: [
