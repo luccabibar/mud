cae80966e860c2ef6d7518aa3775fdf11e75d512
Pedro Muramatsu <pedromuramatsuc@gmail.com>
2019-04-25 08:49:45 -0300

    Esboco Telas MudAnalise II --> Alert e Tabs do Paciente
    25.04.2019

--- mudAnalise/src/app/home/home.page.ts
+++ mudAnalise/src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+import { AlertController } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  constructor(private AlertController: AlertController) {
+
+  }
 
   ngOnInit() {
   }
+  async deletadoSucesso(){
+    const alert = await this.AlertController.create({
+      header: '',
+      subHeader: '',
+      message: "Paciente Deletado com Sucesso",
+      buttons: ['OK']
+
+    });
+    await alert.present();
+  }
+
+  
+
+  async alertaDeletar() {
+    const alert = await this.AlertController.create({
+      header: 'Apagar Resgistro',
+      message: 'Deseja realmente apagar todos os dados deste <strong>paciente</strong>?',
+      buttons: [
+        {
+          text: 'Não',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Sim',
+          handler: () => {
+            this.deletadoSucesso();
+          }
+        }
+      ]
+
+    });
+    await alert.present();
+  }
 
 }
+
+
