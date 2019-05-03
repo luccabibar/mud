ea9ccdf888608beed7de3993ae9acc56837f509f
Pedro Muramatsu <pedromuramatsuc@gmail.com>
2019-05-03 10:13:40 -0300

    Nova tela Psicologo

--- mudAnalise/src/app/tab1/tab1.page.ts
+++ mudAnalise/src/app/tab1/tab1.page.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
+import { CallNumber } from '@ionic-native/call-number/ngx';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+  constructor(
+    private callNumber: CallNumber
+  ) { }
+
+  callNow(number) {
+    this.callNumber.callNumber(number, true)
+      .then(res => console.log('Launched dialer!', res))
+      .catch(err => console.log('Error launching dialer', err));
+  }
+
+}
+
+
