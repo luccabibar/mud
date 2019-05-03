db755d42fbffdebae52b49372a43bdee5b1b891e
Pedro Muramatsu <pedromuramatsuc@gmail.com>
2019-05-03 12:05:38 -0300

    Novo Acordeon
    to programando ainda mas ta tudo indo bem

---
+++ mudAnalise/src/app/components/shared-components.module.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { TimelineComponent } from './timeline/timeline.component';
+
+@NgModule({
+    declarations: [TimelineComponent],
+    imports: [
+        CommonModule
+    ]
+})
+export class SharedComponentsModule{
+
+}
---
+++ mudAnalise/src/app/components/timeline/timeline.component.spec.ts
@@ -0,0 +1,27 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TimelineComponent } from './timeline.component';
+
+describe('TimelineComponent', () => {
+  let component: TimelineComponent;
+  let fixture: ComponentFixture<TimelineComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TimelineComponent ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TimelineComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
