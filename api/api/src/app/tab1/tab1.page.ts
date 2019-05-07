65426bc0040ced65279db0915033ec76fd84e1ae
Giovanni <giovannishibaki@gmail.com>
2019-05-07 09:35:15 -0300

    Teste de DropList

--- api/api/src/app/tab1/tab1.page.ts
+++ api/api/src/app/tab1/tab1.page.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { promise } from 'protractor';
+import { NavController, ToastController } from '@ionic/angular';
+import { Component, ViewChild } from '@angular/core';
+import { ValueAccessor } from '@ionic/angular/dist/directives/control-value-accessors/value-accessor';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+   
+}
