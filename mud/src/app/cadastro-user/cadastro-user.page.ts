1b7e9e218dd54d734b60f5fdedcb06f5b01658a2
Hugo Ferreira <huugo.vieira49@gmail.com>
2019-05-07 08:39:42 -0300

    altera cadastro

---
+++ mud/src/app/cadastro-user/cadastro-user.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-user',
+  templateUrl: './cadastro-user.page.html',
+  styleUrls: ['./cadastro-user.page.scss'],
+})
+export class CadastroUserPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
