eff589d4744ea539e51cc87c9cd00239e84de4eb
Hugo Ferreira <huugo.vieira49@gmail.com>
2019-05-11 12:07:03 -0300

    ta com erro

--- mud/src/app/cadastro-user/cadastro-user.module.ts
+++ mud/src/app/cadastro-user/cadastro-user.module.ts
@@ -3,13 +3,6 @@ import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
-
-
-
-import { IonicModule } from '@ionic/angular';
-import { BrMaskerModule } from 'br-mask';
-
-
 import { CadastroUserPage } from './cadastro-user.page';
 
 const routes: Routes = [
@@ -25,7 +18,6 @@ const routes: Routes = [
     FormsModule,
     IonicModule,
     RouterModule.forChild(routes),
-    BrMaskerModule
   ],
   declarations: [CadastroUserPage]
 })
