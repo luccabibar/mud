f98c0a04c67b681c1c1c97d5c4220b5e75830613
Marcos Josué Costa Dias <marcosdiasbing@gmail.com>
2019-05-14 08:11:04 -0300

    camera funcionano (marcos bonito)

--- auth/src/app/tab2/tab2.page.ts
+++ auth/src/app/tab2/tab2.page.ts
@@ -1,8 +1,34 @@
 import { Component } from '@angular/core';
+import { ZBar, ZBarOptions } from '@ionic-native/zbar/ngx';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page {
+  zbarOptions:any;
+  scannedResult:any;
+ 
+  constructor(
+    private zbar: ZBar
+  ) {
+ 
+    this.zbarOptions = {
+      flash: 'off',
+      drawSight: false
+    }
+ 
+  }
+ 
+  scanCode(){
+    this.zbar.scan(this.zbarOptions)
+   .then(result => {
+      console.log(result); // Scanned code
+      this.scannedResult = result;
+   })
+   .catch(error => {
+      alert(error); // Error message
+   });
+  }
+}
