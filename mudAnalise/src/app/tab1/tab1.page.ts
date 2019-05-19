39bd08b13f7b38e244641245f0500fca63b8b072
Pedro Muramatsu <pedromuramatsuc@gmail.com>
2019-05-19 17:55:22 -0300

    MER e outro JSON
    aqui começamos uma nova etapa, para desenvolver ....

--- mudAnalise/src/app/tab1/tab1.page.ts
+++ mudAnalise/src/app/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CallNumber } from '@ionic-native/call-number/ngx';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab1',
@@ -8,13 +9,18 @@ import { CallNumber } from '@ionic-native/call-number/ngx';
 })
 export class Tab1Page {
   constructor(
-    private callNumber: CallNumber
+    private callNumber: CallNumber,
+    private router:Router
   ) { }
 
   callNow(number) {
     this.callNumber.callNumber(number, true)
       .then(res => console.log('Launched dialer!', res))
       .catch(err => console.log('Error launching dialer', err));
+  }
+
+  abrirAbas(){
+    this.router.navigateByUrl('/opcoes-menu/ficha-usuario')
   }
 
 }
