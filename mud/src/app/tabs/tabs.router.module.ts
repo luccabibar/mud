ed3038de51848aea838fbdb02991d8be1215416b
Ana Lúcia Rodrigues <anacialucia3@gmail.com>
2019-05-24 12:20:16 -0300

    Login pronto <3
    Tá feio mas tá pronto

--- mud/src/app/tabs/tabs.router.module.ts
+++ mud/src/app/tabs/tabs.router.module.ts
@@ -36,14 +36,14 @@ const routes: Routes = [
       },
       {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/tabs/tab2',
         pathMatch: 'full'
       }
     ]
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/tabs/tab2',
     pathMatch: 'full'
   }
 ];
