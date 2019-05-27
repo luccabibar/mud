e613a0db94429ac99fd7d3584e1946ff6bb17fee
Pedro Muramatsu <pedromuramatsuc@gmail.com>
2019-05-27 17:30:48 -0300

    Apresentacao II

--- mudAnalise/src/app/components/modal/modal.page.ts
+++ mudAnalise/src/app/components/modal/modal.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-modal',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModalPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { 
+    
+  }
+
 
   ngOnInit() {
   }
+
+  async voltaTimeline() {
+    this.router.navigateByUrl('/home');
+
+     
+  }
+
 
 }
