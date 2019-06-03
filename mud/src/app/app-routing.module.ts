394603e2d48c0ffcd42ba33a53ce77738da38e2f
Ana Lúcia Rodrigues <anacialucia3@gmail.com>
2019-06-03 11:46:39 -0300

    Relatório semanal
    Iniciando desenvolvimento da página

--- mud/src/app/app-routing.module.ts
+++ mud/src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'tabs', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
   { path: 'tab1', loadChildren: './tab1/tab1.module#Tab1PageModule' },
   { path: 'tab2', loadChildren: './tab2/tab2.module#Tab2PageModule' },
   { path: 'tab3', loadChildren: './tab3/tab3.module#Tab3PageModule' },
   { path: 'load', loadChildren: './load/load.module#LoadPageModule' },
   { path: 'cadastro', loadChildren: './cadastro-user/cadastro-user.module#CadastroUserPageModule' },
-  { path: '', loadChildren: './login-page/login-page.module#LoginPagePageModule' }
+  { path: 'login', loadChildren: './login-page/login-page.module#LoginPagePageModule' },
+  { path: 'relatorio-semanal', loadChildren: './relatorio-semanal/relatorio-semanal.module#RelatorioSemanalPageModule' }
+
   
 ];
 
