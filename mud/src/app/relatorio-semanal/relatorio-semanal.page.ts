4fe26003888a19752dcca1ef923df0f7da37564a
Giovanni Shibaki <giovannishibaki@gmail.com>
2019-07-02 23:07:57 -0300

    Relatorio semanal - estilo + teste api
    estilização da página juntamente com o teste junto da API

--- mud/src/app/relatorio-semanal/relatorio-semanal.page.ts
+++ mud/src/app/relatorio-semanal/relatorio-semanal.page.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
 
 @Component({
   selector: 'app-relatorio-semanal',
   templateUrl: './relatorio-semanal.page.html',
   styleUrls: ['./relatorio-semanal.page.scss'],
 })
+
 export class RelatorioSemanalPage implements OnInit {
+  @ViewChild(IonSlides) IonSlides: IonSlides;
 
   constructor() { }
 
   ngOnInit() {
+    this.IonSlides.lockSwipes(false);
   }
 
+  alimentacao()
+  {
+    this.IonSlides.lockSwipes(false);
+    this.IonSlides.slideTo(1);
+    this.IonSlides.lockSwipes(true);
+  }
+  
+  bem_estar()
+  {
+
+  }
+
+  atividade_fisica()
+  {
+
+  }
+
+  sono()
+  {
+
+  }
 }
