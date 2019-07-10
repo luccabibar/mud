b5682e12cde85b8f338785bc17cb9362134cb0b9
anahu <anacialucia3@gmail.com>
2019-07-10 01:19:08 -0300

    Relatório semanal - alimentação

--- mud/src/app/app-routing.module.ts
+++ mud/src/app/app-routing.module.ts
@@ -3,10 +3,10 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [ 
   { path: 'login-page', loadChildren: './login-page/login-page.module#LoginPagePageModule' },
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'tabs', loadChildren: './tabs/tabs.module#TabsPageModule' },
   { path: 'load', loadChildren: './load/load.module#LoadPageModule' },
   { path: 'cadastro', loadChildren: './cadastro-user/cadastro-user.module#CadastroUserPageModule' },
-  { path: 'relatorio-semanal', loadChildren: './relatorio-semanal/relatorio-semanal.module#RelatorioSemanalPageModule' },
+  { path: '', loadChildren: './relatorio-semanal/relatorio-semanal.module#RelatorioSemanalPageModule' },
   { path: 'relatorio-crise', loadChildren: './relatorio-crise/relatorio-crise.module#RelatorioCrisePageModule' }
   
 ];
