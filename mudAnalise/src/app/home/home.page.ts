791f67bb0ee1662c5838be5731bfeccd700b3c9b
Unknown <pedromuramatsuc@gmail.com>
2019-07-20 18:52:03 -0300

    perfil pronto

--- mudAnalise/src/app/home/home.page.ts
+++ mudAnalise/src/app/home/home.page.ts
@@ -2,8 +2,6 @@ import { AlertController } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
 import { DadosService } from '../servicos/dados.service';
 
-import { ActivatedRoute, Router } from '@angular/router';
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
@@ -17,28 +15,15 @@ export class HomePage implements OnInit {
     {nome:"paciente3",img:"https://cdn.iconscout.com/icon/free/png-256/avatar-375-456327.png"},
     
   ];
-  
   public addSessao(){
-
-    this.router.navigate(["/auth"]);
+    let sessao = {nome:"paciente4", img:"https://cdn.iconscout.com/icon/free/png-256/avatar-375-456327.png"};
+    this.sessoes.push(sessao);
     
-    // let sessao = {nome:"paciente4", img:"https://cdn.iconscout.com/icon/free/png-256/avatar-375-456327.png"};
-    // this.sessoes.push(sessao);
 
   }
 
-  constructor(private AlertController: AlertController, private dados: DadosService, private router: Router, private route: ActivatedRoute) {
+  constructor(private AlertController: AlertController, private dados: DadosService) {
     this.user= this.dados.getDados("user");
-
-    //check para adicionar sessao
-    this.route.queryParams.subscribe(params => {
-      if (this.router.getCurrentNavigation().extras.state) {
-        let sessionDados = this.router.getCurrentNavigation().extras.state;
-        console.log(sessionDados);
-
-      }
-
-    });
   }
 
   ngOnInit() {
