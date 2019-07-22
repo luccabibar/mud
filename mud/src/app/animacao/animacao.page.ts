7d7f83c927142908ba22bc2042051dd72e9bf336
Hugo Ferreira <huugo.vieira49@gmail.com>
2019-07-22 17:32:38 -0300

    conseguindo a animação

--- mud/src/app/animacao/animacao.page.ts
+++ mud/src/app/animacao/animacao.page.ts
@@ -11,13 +11,39 @@ export class AnimacaoPage implements OnInit {
 
   ngOnInit() {
     var counter = 0;
+    var teste = 100;
+    var chegou = 0;
+    var fonte = 13;
+    var opacidade = 100;   //parei aqui!
     var timer = setInterval(function() {
       if( counter >= 10 ) {
         clearInterval( timer );
       }
+      if( chegou == 0){
+        teste+=4;
+        opacidade-=5;
+        fonte+=1;
+        document.getElementById("circulo").style.width=+teste+'px';
+        document.getElementById("circulo").style.height=+teste+'px';
+        document.getElementById("inspire").style.fontSize=+fonte+'px';
+        document.getElementById("inspire").style.opacity=+opacidade+'%';
+        if( teste == 268)
+        {
+          chegou = 1;
+        }
+      }
+      if( chegou == 1){
+        teste-=2;
+        opacidade+=3;
+        document.getElementById("circulo").style.width=+teste+'px';
+        document.getElementById("circulo").style.height=+teste+'px';
+        if(teste == 124)
+        {
+          chegou = 0;
+        }
+      }
       
-      document.getElementById("circulo").style.width='400px';
-    }, 1000);
+    }, 80);
   }
 
 }
