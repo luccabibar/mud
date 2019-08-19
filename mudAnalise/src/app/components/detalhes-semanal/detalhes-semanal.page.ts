0782485df8f088984dc9bd7a4dcfd6b2e31651e0
Pedro Muramatsu <pedromuramatsuc@gmail.com>
2019-08-19 12:26:29 -0300

    detalhes semanal

--- mudAnalise/src/app/components/detalhes-semanal/detalhes-semanal.page.ts
+++ mudAnalise/src/app/components/detalhes-semanal/detalhes-semanal.page.ts
@@ -22,7 +22,7 @@ export class DetalhesSemanalPage implements OnInit {
   myId = null;
   semanaId = null;
   array;
-  existe=1;
+  existe = 1;
 
   constructor(private activatedRoute: ActivatedRoute, public bd: BancoService) {
 
@@ -37,7 +37,7 @@ export class DetalhesSemanalPage implements OnInit {
     this.semanaId = this.array[1];
     this.myId = this.array[0];
   }
-  public ionViewDidEnter(){
+  public ionViewDidEnter() {
     this.myId = this.activatedRoute.snapshot.paramMap.get('myId');
     this.array = this.myId.split("-");
     // divide o array em
@@ -46,22 +46,60 @@ export class DetalhesSemanalPage implements OnInit {
     this.semanaId = this.array[1];
     this.myId = this.array[0];
 
-    if (this.myId == "Sono") {this.pegaSono(); }
+    if (this.myId == "Sono") { this.pegaSono(); }
+    if (this.myId == "Atividade") { this.pegaAtividade(); }
+
 
   }
   //  TODO --> conectar com o banco e puxar detalhe semanal onde semana Id = x && 
   // id usuario =x && id_prof = x
   public async pegaSono() {
     this.bd.selectGenerico("SELECT * FROM sono WHERE semana_id=" + this.semanaId + " ;").then(async (resposta) => {
-      console.log("SONO",resposta)
+      console.log("SONO", resposta)
       this.sono = resposta[0];
-      this.existe=0;
+
+      // ARRUMAR OS t e f que vem do banco 
+
+      // t/s sono
+      if (resposta[0].acordou_naturalmente == 't') {
+        this.sono.acordou_naturalmente = true;
+      } else if (resposta[0].acordou_naturalmente == 'f') {
+        this.sono.acordou_naturalmente = false;
+      }
+      if (resposta[0].acordou == 't') {
+        this.sono.acordou = true;
+      } else if (resposta[0].acordou == 'f') {
+        this.sono.acordou = false;
+      }
+
+      this.existe = 0;
     }).catch(async (resposta) => {
       console.log(resposta);
-      this.existe=2;
+      this.existe = 2;
     })
 
   }
+
+  public async pegaAtividade() {
+    this.bd.selectGenerico("SELECT * FROM atividade_fisica WHERE semana_id=" + this.semanaId + " ;").then(async (resposta) => {
+      console.log("ATIVIDADE FISICA:", resposta)
+      this.sono = resposta[0];
+
+      // ARRUMAR OS t e f que vem do banco 
+
+      // t/s atividade
+      if (resposta[0].realizou == 't') {
+        this.atividade.realizou = true;
+      } else if (resposta[0].realizou == 'f') {
+        this.atividade.realizou = false;
+      }
+      this.existe = 0;
+    }).catch(async (resposta) => {
+      console.log(resposta);
+      this.existe = 2;
+    })
+  }
+
 
 
 
