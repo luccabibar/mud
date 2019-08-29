b2678d5bd49796b56ea24921c721a104786c4dd7
Ana Lúcia Rodrigues <anacialucia3@gmail.com>
2019-08-29 08:54:10 -0300

    NAO MEXER NO RELATORIO SEMANAL
    NAO MEXE

--- mud/src/app/tab2/tab2.page.ts
+++ mud/src/app/tab2/tab2.page.ts
@@ -2,6 +2,9 @@ import { Router, RouterModule } from '@angular/router';
 import { Component, OnInit, Renderer, ViewChild, Input} from '@angular/core';
 import { NavController, AlertController, IonInput} from '@ionic/angular';
 import { DadosService } from '../dados.service';
+import { BancoService } from './../banco.service';
+import { identifierModuleUrl, ReturnStatement } from '@angular/compiler';
+
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
@@ -10,23 +13,27 @@ import { DadosService } from '../dados.service';
 export class Tab2Page {
   
   
-  constructor(private nav: NavController, public alertController: AlertController,   private router: Router, private dadosService: DadosService) {}
+  constructor(private nav: NavController, public alertController: AlertController,   private router: Router, private dadosService: DadosService, private BancoService: BancoService) {}
 
   dataInicio = "TesteInicio";
   dataFinal = "TestFinal";
-
-  pegarUltimoRelatorio()
-  {
-    
-  }
 
   relatcrise()
   {
     this.router.navigateByUrl('/animacao');
   }
+
   async relatsem()
-  {
-    this.pegarUltimoRelatorio();
+  {/*
+    var libera = null;
+    var dataHj =  new Date();
+
+    
+    var diaHj = dataHj.getDay();
+    var mesHj = dataHj.getMonth();
+    libera = this.liberaRelat(dataHj, diaHj, mesHj);
+*/
+
     const alert = await this.alertController.create({
     header: "Relatório Semanal",
     subHeader: "Confirmar Data",
@@ -53,5 +60,114 @@ export class Tab2Page {
     
     await alert.present();
   }
+
+  liberaRelat(alldato, dato, meso)
+  {
+    var dayDif = 0;
+    var dataUlt= "";
+    var diUlt = 0;
+    var meUlt = 0;
+    var difMes = 0;
+    var anoUlt = 0;
+    this.BancoService.selectGenerico("SELECT * FROM semana WHERE usuario_id ='"+this.dadosService. getId()+"'ORDER BY created_at DESC LIMIT 1;")
+    .then(async(Response)=>{
+      dataUlt = Response[0]['data_final'];
+      diUlt= parseInt(dataUlt.substr(8,2));
+      meUlt = parseInt(dataUlt.substr(5,2));
+      difMes = meso - meUlt;
+      anoUlt = parseInt(dataUlt.substr(0,4));
+      dayDif = dato - diUlt;
+      if(difMes == 0)
+      {
+        if(dayDif >= 7)
+        {
+          return true;
+        }
+        else
+        {
+          // aaaaaaaaaaa
+        }
+      }
+      else if(difMes == 1)
+      {
+        if(meUlt == 1|| meUlt == 3 || meUlt == 5 || meUlt == 7 || meUlt == 8 || meUlt == 10 || meUlt == 12)
+        {
+          dayDif = (31 - diUlt) + dato;
+          if(dayDif >= 7)
+          {
+            return true;
+          } 
+          else
+          {
+            /////
+          }
+        }
+        else if(meUlt == 2)
+        {
+          if ((anoUlt % 4 == 0) && ((anoUlt % 100 != 0) || (anoUlt % 400 == 0)))
+          {
+            dayDif = (29 - diUlt) + dato;
+            if(dayDif >= 7)
+            {
+              return true;
+            }
+            else
+            {
+              ///////
+            }
+          }
+          else
+          {
+            dayDif = (28 - diUlt) + dato;
+            if(dayDif >= 7)
+            {
+              return true;
+            }
+            else 
+            {
+              ////
+            }
+          }
+        }
+        else
+        {
+          dayDif = (30 - diUlt) + dato;
+          if(dayDif >= 7)
+          {
+            return true;
+          }
+          else
+          {
+            ///////
+          }
+        }
+      }
+      else
+      {
+        return true;
+      }
+
+    })
+    .catch(async(response)=>{
+
+      const alert = await this.alertController.create({
+        header: 'DEU ERRO',
+        message: 'Não será possível inserir outro mural semanal em menos de uma semana!',
+        buttons:  [
+          {
+            text: 'OK',
+          }
+        ],
+      });
+  
+      await alert.present();
+       })
+    
+  }
+
+
+
+
+
 }
 
