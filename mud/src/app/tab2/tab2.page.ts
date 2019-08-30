5f62d550bf147b39b1db965cae710e9664dd56fb
Ana Lúcia Rodrigues <anacialucia3@gmail.com>
2019-08-30 12:17:33 -0300

    NAO MEXE NO RELATORIO SEMANAL
    NA0 MEXE AAAAAAAA

--- mud/src/app/tab2/tab2.page.ts
+++ mud/src/app/tab2/tab2.page.ts
@@ -4,6 +4,7 @@ import { NavController, AlertController, IonInput} from '@ionic/angular';
 import { DadosService } from '../dados.service';
 import { BancoService } from './../banco.service';
 import { identifierModuleUrl, ReturnStatement } from '@angular/compiler';
+import { parseSelectorToR3Selector } from '@angular/compiler/src/core';
 
 @Component({
   selector: 'app-tab2',
@@ -15,8 +16,8 @@ export class Tab2Page {
   
   constructor(private nav: NavController, public alertController: AlertController,   private router: Router, private dadosService: DadosService, private BancoService: BancoService) {}
 
-  dataInicio = "TesteInicio";
-  dataFinal = "TestFinal";
+  dataInicio = "INICIO";
+  dataFinal = "FINAL";
 
   relatcrise()
   {
@@ -24,20 +25,20 @@ export class Tab2Page {
   }
 
   async relatsem()
-  {/*
+  {
     var libera = null;
     var dataHj =  new Date();
-
-    
-    var diaHj = dataHj.getDay();
+    var anoHj = dataHj.getFullYear();
+    var diaHj = dataHj.getDate();
     var mesHj = dataHj.getMonth();
+    var nam = anoHj + "-" + mesHj + "-" + diaHj;
     libera = this.liberaRelat(dataHj, diaHj, mesHj);
-*/
 
+    //if(mesHj < 10)
     const alert = await this.alertController.create({
     header: "Relatório Semanal",
     subHeader: "Confirmar Data",
-    message: "A data do último relatório enviado foi de:<br><br><b>"+this.dataInicio+"</b><br><br>até:<br><br><b>"+this.dataFinal+"</b><br>",
+    message: "A data do último relatório enviado foi de:<br><br><b>"+libera+"</b><br><br>até:<br><br><b>"+nam+"</b><br>",
 
     buttons: [
       {
@@ -50,8 +51,8 @@ export class Tab2Page {
       {
         text: 'Confirmar',
         handler: data => {
-          this.dadosService.setData_relatorioS_I = data.dataInicio;
-          this.dadosService.setData_relatorioS_F = data.dataFim;
+          this.dadosService.setData_relatorioS_I = data.libera;
+          this.dadosService.setData_relatorioS_F = data.dataHj;
           this.nav.navigateForward('relatorio-semanal');
         }
       }
@@ -63,13 +64,17 @@ export class Tab2Page {
 
   liberaRelat(alldato, dato, meso)
   {
+    var pega = new Date();
+    var anoagr = pega.getFullYear();
     var dayDif = 0;
     var dataUlt= "";
     var diUlt = 0;
     var meUlt = 0;
     var difMes = 0;
     var anoUlt = 0;
-    this.BancoService.selectGenerico("SELECT * FROM semana WHERE usuario_id ='"+this.dadosService. getId()+"'ORDER BY created_at DESC LIMIT 1;")
+    var voltad;
+    var voltaa;
+    this.BancoService.selectGenerico("SELECT * FROM semana WHERE usuario_id ='"+this.dadosService.getId()+"'ORDER BY created_at DESC LIMIT 1;")
     .then(async(Response)=>{
       dataUlt = Response[0]['data_final'];
       diUlt= parseInt(dataUlt.substr(8,2));
@@ -77,25 +82,29 @@ export class Tab2Page {
       difMes = meso - meUlt;
       anoUlt = parseInt(dataUlt.substr(0,4));
       dayDif = dato - diUlt;
-      if(difMes == 0)
+      if(difMes == 0 && anoUlt == anoagr)
       {
         if(dayDif >= 7)
         {
-          return true;
+          voltad = dato - 7;
+          voltaa = anoagr + "-" + meso + "-" + voltad;
+          return voltaa;
         }
         else
         {
           // aaaaaaaaaaa
         }
       }
-      else if(difMes == 1)
+      else if(difMes == 1 && anoUlt == anoagr)
       {
         if(meUlt == 1|| meUlt == 3 || meUlt == 5 || meUlt == 7 || meUlt == 8 || meUlt == 10 || meUlt == 12)
         {
           dayDif = (31 - diUlt) + dato;
           if(dayDif >= 7)
           {
-            return true;
+            voltad = 31 - (7 - dato);
+            voltaa = anoagr + "-" + meso-- + voltad;
+            return voltaa;
           } 
           else
           {
@@ -109,7 +118,9 @@ export class Tab2Page {
             dayDif = (29 - diUlt) + dato;
             if(dayDif >= 7)
             {
-              return true;
+              voltad = 29 - (7 - dato);
+              voltaa = anoagr + "-" + meso-- +"-" + voltaa;
+              return voltaa;
             }
             else
             {
@@ -121,7 +132,9 @@ export class Tab2Page {
             dayDif = (28 - diUlt) + dato;
             if(dayDif >= 7)
             {
-              return true;
+              voltad = 28 - (7 - dato);
+              voltaa = anoagr + "-" + meso-- +"-" + voltaa;
+              return voltaa;
             }
             else 
             {
@@ -134,7 +147,9 @@ export class Tab2Page {
           dayDif = (30 - diUlt) + dato;
           if(dayDif >= 7)
           {
-            return true;
+            voltad = 30 - (7 - dato);
+            voltaa = anoagr + "-" + meso +"-" + voltaa;
+            return voltaa;
           }
           else
           {
@@ -144,10 +159,53 @@ export class Tab2Page {
       }
       else
       {
-        return true;
+        if(dato > 7)
+        {
+          voltad = dato - 7;
+          voltaa = anoagr + "-" + meso + "-" + voltad;
+          return voltaa;
+        }
+        else 
+        {
+          if(meso == 5 || meso == 7 || meso == 10 || meso == 12)
+          {
+            voltad = 30 - (7 - dato);
+            voltaa = anoagr + "-" + meso-- + "-" + voltad;
+            return voltaa;
+          }
+          else if(meso == 3)
+          {
+              if ((anoagr % 4 == 0) && ((anoagr % 100 != 0) || (anoagr % 400 == 0)))
+              {
+                  voltad = 29 - (7 - dato);
+                  voltaa = anoagr + "-" + meso-- + "-" + voltad;
+                  return voltaa;
+              }
+              else
+              {
+                  voltad = 28 - (7 - dato);
+                  voltaa = anoagr + "-" + meso-- + "-" + voltad;
+                  return voltaa;
+              }
+          }
+          else
+          { 
+              voltad = 31 - (7 - dato);
+              if(meso == 1)
+              {
+                  voltaa = anoagr-- + "-" + 12 + "-" + voltad;
+                  return voltaa;
+              }
+              else
+              {
+                  voltaa = anoagr + "-" + meso-- + "-" + voltad;
+                  return voltaa;
+              }
+          }
       }
 
-    })
+    }
+  })
     .catch(async(response)=>{
 
       const alert = await this.alertController.create({
