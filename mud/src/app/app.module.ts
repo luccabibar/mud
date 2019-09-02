94f84fb2c2033cf87fa3fda421cd898e25b27347
João Pedro Pilastri Terruel <joaopedropilastri@gmail.com>
2019-09-02 17:30:15 -0300

    Notificacao

--- mud/src/app/app.module.ts
+++ mud/src/app/app.module.ts
@@ -3,23 +3,39 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { Firebase } from '@ionic-native/Firebase';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+import { FcmnoticatiosService } from './fcmnoticatios.service';
 
+const firebase = {
+  apiKey: "AIzaSyDns6l1bdVQVtTNmPpeFVk8LBauDCKKiuk",
+    authDomain: "mud-notificacao.firebaseapp.com",
+    databaseURL: "https://mud-notificacao.firebaseio.com",
+    projectId: "mud-notificacao",
+    storageBucket: "",
+    messagingSenderId: "465038185208",
+    appId: "1:465038185208:web:19b85f7a30ffa2a4"
 
+ }
+ 
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, AngularFireModule.initializeApp(firebase), 
+    AngularFirestoreModule,],
   providers: [
     StatusBar,
     SplashScreen,
+    FcmnoticatiosService,
     BarcodeScanner,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     ScreenOrientation
