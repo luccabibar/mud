944749eeafb38009b7113945cfc19f92e03079dd
--help <ra1757052@CTIBAURU.FEB.UNESP.BR>
2019-09-02 12:21:37 -0300

    inicio pag grafico do relatorio semanal

--- mudAnalise/src/app/opcoes-menu/grafico-semanal/grafico-semanal.page.ts
+++ mudAnalise/src/app/opcoes-menu/grafico-semanal/grafico-semanal.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { BancoService } from '../../servicos/banco.service';
+import { DadosService } from '../../servicos/dados.service';
+
 @Component({
   selector: 'app-grafico-semanal',
   templateUrl: './grafico-semanal.page.html',
@@ -7,7 +10,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GraficoSemanalPage implements OnInit {
 
-  constructor() { }
+  constructor(private db: BancoService, private data: DadosService) { }
 
   ngOnInit() {
   }
