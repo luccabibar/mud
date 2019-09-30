94178a7d9a2224c93bd209725610f85433f0672e
Hugo Ferreira <huugo.vieira49@gmail.com>
2019-09-30 12:10:18 -0300

    arrumei para a tela nn girar

--- mudAnalise/src/app/app.module.ts
+++ mudAnalise/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
@@ -28,7 +29,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     CallNumber,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     DadosService,
-    
+    ScreenOrientation
   ],
   bootstrap: [AppComponent]
 })
