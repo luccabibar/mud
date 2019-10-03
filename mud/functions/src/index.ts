467acac35811d1c54b528319c458f6654f024a54
Hugo Ferreira <huugo.vieira49@gmail.com>
2019-10-03 08:47:51 -0300

    hugo mexendo

--- mud/functions/src/index.ts
+++ mud/functions/src/index.ts
@@ -1,4 +1,5 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -6,3 +7,20 @@ import * as functions from 'firebase-functions';
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+/* export const sendOnFireStoreCreate = functions.firestore
+ .document('discounts/{discountId}')
+ .onCreate(async snapshot => {
+    const discount = snapshot.data();
+
+    const notification: admin.messaging.Notification = {
+        title: 'New Hugo Available',
+        body: discount.headline
+    }; 
+
+    const payload: admin.messaging.Message = {
+        notification,
+        topic: 'discounts'
+    };
+    return admin.messaging().send(payload);
+ });*/
