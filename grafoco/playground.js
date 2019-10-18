4477b966a51f8e38f1ce14b974c76cfb352e4b1e
Lucca Tadeu Silva Bibar <ra1757052@CTIBAURU.FEB.UNESP.BR>
2019-10-18 12:04:56 -0300

    progresso nos graficos (conseugui cirar um grafico de barras com cores diferentes baseados numa condicao)

--- grafoco/playground.js
+++ grafoco/playground.js
@@ -1,45 +1,60 @@
-window.onload = () =>
+function loadStuff ()
 {
     let dataset = [];
-    let data = [0, 1, 2, 3, 10, 5, 7, 3, 10];
-    let colors = [true, false, true, false, true, false, false, false, true];
-    let customColors = ['3399ff', 'ff3333'];
+    let data = [0, 1, 2, 3, 10, 5, 7, 3, 11];
+    let colorSet = ['3399ff', 'ff3333'];
         
     for (let i = 0; i < data.length; i++) {
-        let color = cutsomColors[(colors[i]) ? 0 : 1];
+
+        let condition = (Math.floor(Math.random() * 100000) % 2 == 0) ? true : false;
+  
         if(i == 0){
-            dataset = {
+            dataset.push({
                 label: "frequencia",
-                data: data[i],
-                borderColor: '#' + color + 'ff',
-                backgroundColor: '#' + color + '99',
-                fill: false,
+                data: [(condition) ? data[i] : 0],
+                borderColor: '#' + colorSet[0] + 'ff',
+                backgroundColor: '#' + colorSet[0] + '99',
+                fill: true,
                 borderWidth: 1
-            }       
+            });  
+            dataset.push({
+                label: "frequencia",
+                data: [(!condition) ? data[i] : 0],
+                borderColor: '#' + colorSet[1] + 'ff',
+                backgroundColor: '#' + colorSet[1] + '99',
+                fill: true,
+                borderWidth: 1
+            });        
         }
         else{
-            dataset.data.push(data[i]);
-            dataset.data.push(color);
-            dataset.data.push(color);
+            dataset[0].data.push((condition) ? data[i] : 0);
+            dataset[1].data.push((!condition) ? data[i] : 0);
         }        
     }
 
     let grafStuff = {
 		type: 'bar',
 		data: {
-			//labels: ['S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8'],
-			datasets: dataset.data
+			labels: ['S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9'],
+			datasets: dataset
 		},
 		options: {
 			scales: {
-			yAxes: [{
-				ticks: {
-				beginAtZero: true
-				}
-			}]
-			}
+                xAxes: [{
+                    stacked: true
+                }],
+                yAxes: [{
+                    stacked: true
+                }]
+            }
 		}
     }
     
     grafObj = new Chart($("#graf")[0], grafStuff);
+    
+}
+
+window.onload = () =>
+{
+    loadStuff();
 }
