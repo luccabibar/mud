930c77166445fed98dca5643596364078c04e2de
bibar <luccabibar@gmail.com>
2019-10-27 19:17:20 -0300

    pagina de crises 100% (mas se alguem se habilitar a testar mais a fundo seria top)

--- mudAnalise/src/app/opcoes-menu/grafico-crise/grafico-crise.page.ts
+++ mudAnalise/src/app/opcoes-menu/grafico-crise/grafico-crise.page.ts
@@ -15,33 +15,45 @@ export class GraficoCrisePage implements OnInit
 {
   //dados do usuario
   dadosUser;
-  //controle da pagina
-  dias = 30; //ate qunatos dias ele pega no banco
   //grafico
   @ViewChild('grafico') grafElem;
   grafObj;
+  //estatisticas
+  estatisticas
   //view
   error;
 
   /**
    * carrega o grafico 
    */
-  async loadGraf()
+  async loadView()
   {
-    //pega os dados brutos do banco
-    let response: any = await this.getData(this.dadosUser.id_usuario); 
-    if(!response.sucesso){
-      console.log(response.dados);
+    try{
+      //pega os dados brutos do banco
+      let response: any = await this.getData(this.dadosUser.id_usuario); 
+      if(!response.sucesso){
+        console.log(response.dados);
+        this.error = true;
+        return;
+      }
+      let data = response.dados
+                                                              
+      //processa os dados do banco em estatisticas e carrega elas pra view
+      this.estatisticas = this.prepareEstatisticas(data);
+      console.log(this.estatisticas);      
+
+      //processa os dados do banco em um dataset pro grafico
+      let grafStuff = this.prepareGrafStuff(data);
+      console.log(grafStuff);
+      
+      //inicializa grfico com dados processado
+      this.grafObj = new Chart(this.grafElem.nativeElement, grafStuff);
+    }
+    catch(ex){
+      console.log(ex);
       this.error = true;
     }
-    let data = response.dados
-                                                             
-    //processa os dados do banco
-    let grafStuff = this.prepareGrafStuff(data);
-    //inicializa grfico com dados processado
-    this.grafObj = new Chart(this.grafElem.nativeElement, grafStuff);
 
-    // loadObservacoes(grafData);
   }
 
   /**
@@ -57,11 +69,10 @@ export class GraficoCrisePage implements OnInit
     {
       let dataset = null;
       //query sql
-      //seleciona as crises que pertencam a um usuario E seja dos ultimos 30 dias, agrupando por intensidade E dia que ocorreu
-      let sql = "SELECT COUNT(created_at) AS quantidade, created_at AS data, intensidade " +
+      //seleciona as crises que ocorreram no ultimo ano
+      let sql = "SELECT created_at AS data " +
         "FROM public.crise WHERE usuario_id = " + id + " AND " +
-        "DATE_PART('day', now() - created_at) < " + this.dias + " " +
-        "GROUP BY (created_at, intensidade) " +
+        "DATE_PART('day', now() - created_at) < " + 365 + " " +
         "ORDER BY data;";
       this.db.selectGenerico(sql)
       //sucesso
@@ -79,23 +90,77 @@ export class GraficoCrisePage implements OnInit
   }
 
   /**
-   * prepara todas as informacoes que um grafico precisa com base em dados brutos do banci
+   * prepara dados estatisticos com base na quantidade de crises
+   * 
+   * @param data dados brutos base
+   * @returns objeto de dados processados
+   */
+  prepareEstatisticas(data)
+  {
+    let total = 0;
+
+    //conta total de crises;
+    data.forEach(element => 
+    {
+      total++;
+    });
+
+    let mediaMensal = (total / 12).toFixed(2);
+    let mediaDiaria = (total / 365).toFixed(2);
+    let mediaInterv = (total != 0) ? Math.round(365 * 1 / total) : false;
+
+    return {
+      total: total,
+      mediaMensal: mediaMensal,
+      mediaDiaria: mediaDiaria,
+      mediaInterv: mediaInterv
+    };
+  }
+
+  /**
+   * prepara todas as informacoes que um grafico precisa com base em dados brutos do banco
    * 
    * @param data dados brutos do banco
    */
   prepareGrafStuff(data)
   {
     let dataset = this.prepareDataset(data);
-    // let labels = [];// this.prepareLabels(data);
+    let labels = this.prepareLabels();    
 
     let opts = {
+      legend: {
+        display: true,
+        position: 'bottom',
+        align: "start"
+      },
+
+      scales: {
+        xAxes: [{
+          stacked: false,
+          scaleLabel: {
+            display: false,
+            labelString: 'tempo'
+          }
+        }],
+        yAxes: [{
+          stacked: false,
+          ticks : {
+              suggestedMax : 10,
+              min : 0
+          },
+          scaleLabel: {
+            display: true,
+            labelString: 'crises por mês'
+          }
+        }]
+      }
 
     };
 
     return {
-      type: 'bubble',
+      type: 'bar',
       data: {
-        // labels: labels,
+        labels: labels,
         datasets: dataset
       },
       options: opts
@@ -106,80 +171,82 @@ export class GraficoCrisePage implements OnInit
    * processa dados brutos do banco em um dataset para o grafico
    * 
    * @param data dados brutos dos bancos
+   * @return dataset processado
+   * 
    */
   prepareDataset(data)
   {
-    let colors = ['d62a61'];
-    let ultimosNDias = this.getUltimosNDias(this.dias);
-    let dataset = {
-      data: [],
-      borderColor: "#" + colors[0] + "ff",
-      backgroundColor: "#" + colors[0] + "99",
-    };
+    let colors = ['D62A61'];
+    let ultimoAno = [];
+    let hoje  = new Date();
+    
+    for (let i = 0; i < 13; i++) {
+      ultimoAno.push(0);      
+    }
 
     //itera sobre os N ultimos dias
-    for(let i = 0; i < ultimosNDias.length; i++){
-      let found = false;
-      //procura crises que tenham ocorrido nesta data 
-      data.forEach((crise) => 
-      {
-        //se achar uma crise que tenha ocorrido nesse dia, add ao dataset        
-        if(crise.data == this.formatData(ultimosNDias[i])){
-          found = true;
-          console.log("foundk", i);
-          
-          dataset.data.push({
-            x: i + 1,
-            y: Number(crise.intensidade),
-            r: crise.quantidade * 5
-          });
-        }
-      });
-
-      //se nao achar nenhuma crise, add dado vazio para ocupar espaco
-      if(!found){
-        dataset.data.push({
-          x: i + 1,
-          y: 0,
-          r: 0
-        });
-      }
-    }
-    console.log(dataset);
-    return dataset;
-  }
-
-  getUltimosNDias(qtd)
-  {
-    let lastN = [];
-    let hj = new Date();
-
-    for(let i = 0; i < qtd; i++){
-      //milisegundos desde time 0 ate a data de i dias atras
-      let NDiasAtras = new Date().setDate(hj.getDate() - i);
-      lastN.push(new Date(NDiasAtras));
-    }
-
-    return lastN.reverse();
+    data.forEach(crise => {
+      //adiciona +1 no array de crises para cada crise que ocorre num mes
+      data = new Date(crise.data);
+      ultimoAno[this.diffMeses(hoje, data)]++;
+    });
+    
+    return [{
+      label: 'crises ocorridas',
+      data: ultimoAno.reverse(),
+      borderColor: "#" + colors[0] + "FF",
+      backgroundColor: "#" + colors[0] + "99",
+    }];
   }
 
   /**
-   * formata um objeto data para que fique igual ao padrao do postgresql
+   * prepara os labels para o grafico
    * 
-   * @param data objeto data
-   * @returns string semenlhande ao padrao do postgresql
+   * @returns o array de labels
    */
-  formatData(data)
+  prepareLabels()
   {
-    return data.getFullYear() + "-" + (data.getMonth() + 1) + "-" + data.getDate();
+    let meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+    let hoje  = new Date();
+    let ano = hoje.getFullYear();
+    let labels = [];
+
+    for(let i = 0; i < 13; i++){
+      let mes = hoje.getMonth() - i;
+      //if ano atual
+      if(mes >= 0){
+        labels.push(meses[mes] + " de " + ano);
+      }
+      //else ano passado
+      else{
+        labels.push(meses[mes + 12] + " de " + (ano - 1));
+      }
+    }
+
+    return labels.reverse();
+  }
+
+  /**
+   * cacula a diferenca de meses de duas datas
+   * 
+   * @param dataX primeira data
+   * @param dataY segunda data
+   * @returns a diferenca
+   */
+  diffMeses(dataX, dataY)
+  {
+    let yearDiff = dataX.getFullYear() - dataY.getFullYear();  
+    let diferenca = dataX.getMonth() - dataY.getMonth();
+    return diferenca + ((yearDiff == 0) ? 0 : 12);
   }
 
   constructor(private db: BancoService, private ds: DadosService, private router: Router) 
   {
     this.error = false;
     this.grafObj = null;
+    this.estatisticas = null;
     this.dadosUser = this.ds.getDados("user_sessao");
-    this.loadGraf();
+    this.loadView();
   }
 
   ngOnInit() 
